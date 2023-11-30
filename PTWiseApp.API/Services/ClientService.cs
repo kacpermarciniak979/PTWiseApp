cecacbfda710272d1c3ba5f0308f15ecea1558d9
Throupy <owenthroup@gmail.com>
2023-11-30 13:57:07 +0000

    Created functionality for retrieving Clients, including getting a singular client by Id. Note: Something is amiss with the entity relations - there is no one-to-many relation between trainer and clients, for example. Will look into this. Updated .gitignore to exclude appsettings.json file.

---
+++ PTWiseApp.API/Services/ClientService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PTWiseApp.API.Data;
+using PTWiseApp.API.Entities;
+
+namespace PTWiseApp.API.Services
+{
+    public class ClientService
+    {
+        private readonly PTWiseDbContext pTWiseDbContext;
+
+        public ClientService(PTWiseDbContext pTWiseDbContext)
+        {
+            this.pTWiseDbContext = pTWiseDbContext;
+        }
+
+        public async Task<List<Client>> GetClientsAsync()
+        {
+            var result = await pTWiseDbContext.Clients.ToListAsync();
+            return result;
+        }
+
+        public async Task<Client> GetClientByIdAsync(int Id)
+        {
+            var result = await pTWiseDbContext.Clients
+                .FirstOrDefaultAsync(x => x.Id == Id);
+            return result;
+        }
+    }
+}
---
+++ PTWiseApp/Services/ClientService.cs
@@ -0,0 +1,41 @@
+﻿using PTWiseApp.API.Entities;
+using PTWiseApp.Web.Services.Interfaces;
+
+namespace PTWiseApp.Web.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Client>> GetClientsAsync()
+        {
+            try
+            {
+                var result = await _httpClient.GetFromJsonAsync<IEnumerable<Client>>("api/Client/GetClients");
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<Client> GetClientByIdAsync(int Id)
+        {
+            try
+            {
+                var result = await _httpClient.GetFromJsonAsync<Client>($"api/Client/GetClientById?Id={Id}");
+                return result;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
